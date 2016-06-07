76c44f40373f0581d4178726febee693397c87d2
Skite <Skite@mail.ithome.com.tw>
2016-06-07 18:30:56 +0800

    renew ship and earth size

--- js/app.js
+++ js/app.js
@@ -421,8 +421,8 @@
             yellow = 255 * Math.random();
             star = new Star(scene, x, y, radius, radius, 'star', yellow);
         }
-        ship = new Ship(scene, 150, 100, 220, 80, 'shippy');
-        earth = new Earth(scene, 950, 200, 145, 145, 'earthy');
+        ship = new Ship(scene, 150, 100, 305, 180, 'shippy');
+        earth = new Earth(scene, 800, 50, 300, 300, 'earthy');
         return setTimeout(function() {
             return scene.target.y = 90;
         }, 1000);
